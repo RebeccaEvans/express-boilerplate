2a62688a03da559c51c32e4368d0c142f72059b9
RebeccaEvans <evans.rebeccaj@gmail.com>
2019-12-12 16:09:35 -0800

    additional tweaks to views and controllers

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+// Required node modules
 let express = require('express')
 let layouts = require('express-ejs-layouts')
 
+// Declare express app variable
 let app = express()
 
-//set up middleware
+// Set up and middleware
 app.set('view engine', 'ejs')
 app.use(layouts)
 app.use('/', express.static('static'))
@@ -11,15 +13,15 @@ app.use('/', express.static('static'))
 // Add any controllers we have
 app.use('/auth', require('./controllers/auth'))
 
-//add home or catch-all routes
+// Add home or catch-all routes
 app.get('/', (req, res) => {
-	res.render('home')
+  res.render('home')
 })
 
 app.get('*', (req, res) => {
-	res.render('error404')
+  res.render('error404')
 })
 
-app.listen(8000, () => {
-	console.log('You are connected, good job.')
+app.listen(3000, () => {
+  console.log('Hello and good morning! ☕️')
 })
